5f566c4b90474f3dddf1a0903eb743093d5ab95b
Alizia <roimichael70@hotmail.fr>
2017-09-29 16:14:02 +0200

    Fix maj 1

--- mangos-classic/src/shared/revision_sql.h
+++ mangos-classic/src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_z2678_01_realmd"
- #define REVISION_DB_CHARACTERS "required_z2698_01_characters_playerbot_saved_data"
- #define REVISION_DB_MANGOS "required_z2702_01_mangos_spell_chain_totems_typos"
+ #define REVISION_DB_CHARACTERS "required_z2708_01_characters_account_instances_entered"
+ #define REVISION_DB_MANGOS "required_z2710_01_mangos_questgiver_greeting"
 #endif // __REVISION_SQL_H__
